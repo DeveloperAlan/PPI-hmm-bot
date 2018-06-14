61b656505940ccf92e3d4c3cd165b49b30475ed4
Alan Ng <alan.ng@blake.com.au>
2018-06-14 19:40:59 +1000

    added light code v1

--- hmm_bot_console.py
+++ hmm_bot_console.py
@@ -8,21 +8,90 @@ import time
 import pygame
 import random
 import os
+import RPi.GPIO as GPIO
 
 #Array that  references sounds
 hmm = ["./sounds/dunky_hmm.wav","./sounds/hmm.wav","./sounds/nootnoot.wav","./sounds/pewdipiehmmlaugh.wav","./sounds/siegmeyermmm.wav","./sounds/spongebobhmm.wav","./sounds/Yodahmm.wav"]
 
+redPin = 11
+greenPin = 13
+bluePin = 15
+
+def turnOn(pin):
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(pin, GPIO.OUT)
+	GPIO.output(pin, GPIO.HIGH)
+
+def turnOff(pin):
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(pin, GPIO.OUT)
+	GPIO.output(pin, GPIO.LOW)
+
+def redOn():
+	turnOn(redPin)
+
+def greenOn():
+	turnOn(greenPin)
+
+def blueOn():
+	turnOn(bluePin)
+
+def yellowOn():
+	turnOn(redPin)
+	turnOn(greenPin)
+
+def cyanOn():
+	turnOn(greenPin)
+	turnOn(bluePin)
+
+def magentaOn():
+	turnOn(redPin)
+	turnOn(bluePin)
+
+def whiteOn():
+	turnOn(redPin)
+	turnOn(greenPin)
+	turnOn(bluePin)
+
+def redOff():
+	turnOff(redPin)
+
+def greenOff():
+	turnOff(greenPin)
+
+def blueOff():
+	turnOff(bluePin)
+
+def yellowOff():
+	turnOff(redPin)
+	turnOff(greenPin)
+
+def cyanOff():
+	turnOff(greenPin)
+	turnOff(bluePin)
+
+def magentaOff():
+	turnOff(redPin)
+	turnOff(bluePin)
+
+def whiteOff():
+	turnOff(redPin)
+	turnOff(greenPin)
+	turnOff(bluePin)
 
 def choose_action(x):
     if x == "1":
         print("Okay 1")
         pygame.mixer.music.load(hmm[0])
         pygame.mixer.music.play()
+        redOn()
     elif x == "2":
         print("Okay 2")
         pygame.mixer.music.load(hmm[1])
         pygame.mixer.music.play()
+        greenOn()
     else:
+        blueOn()
         print("Not Okay")
 
 print('program currently yrunning')
