5148db34306f7bf2e3924ede6a87b4e76e497a13
Alan Ng <alan.ng@blake.com.au>
2018-06-14 18:45:07 +1000

    added the hmm bot console for debugging purposes

--- python_test.py
+++ python_test.py
@@ -5,29 +5,22 @@
 import logging
 import sys
 import time
-import pygame
 import random
 import os
 
 def choose_action(x):
-    if x == 1:
+    if x == "1":
         print("Okay 1")
-    elif x == 2:
+    elif x == "2":
         print("Okay 2")
     else:
         print("Not Okay")
 
 print('program currently yrunning')
-pygame.mixer.init()
-print("pygame has finished initializing")
 #Array that  references sounds
 hmm = ["./sounds/dunky_hmm.wav","./sounds/hmm.wav","./sounds/nootnoot.wav","./sounds/pewdipiehmmlaugh.wav","./sounds/siegmeyermmm.wav","./sounds/spongebobhmm.wav","./sounds/Yodahmm.wav"]
 
-pygame.mixer.music.load(random.choice(hmm))
-print("pygame has loaded all the hmm")
-
 while True:
-    READ=os.read(0, 100)
-    print(READ)
+    READ=raw_input("which input\n")
     choose_action(READ)
 
