8591cdf1fa47358c06daf49f714509d6918b27ae
Alan Ng <alan.ng@blake.com.au>
2018-06-14 19:02:10 +1000

    added sounds to the debugger console

--- hmm_bot_console.py
+++ hmm_bot_console.py
@@ -9,25 +9,28 @@ import pygame
 import random
 import os
 
+#Array that  references sounds
+hmm = ["./sounds/dunky_hmm.wav","./sounds/hmm.wav","./sounds/nootnoot.wav","./sounds/pewdipiehmmlaugh.wav","./sounds/siegmeyermmm.wav","./sounds/spongebobhmm.wav","./sounds/Yodahmm.wav"]
+
+
 def choose_action(x):
     if x == 1:
         print("Okay 1")
+        pygame.mixer.music.load(hmm[0])
     elif x == 2:
         print("Okay 2")
+        pygame.mixer.music.load(hmm[1])
     else:
         print("Not Okay")
 
 print('program currently yrunning')
 pygame.mixer.init()
 print("pygame has finished initializing")
-#Array that  references sounds
-hmm = ["./sounds/dunky_hmm.wav","./sounds/hmm.wav","./sounds/nootnoot.wav","./sounds/pewdipiehmmlaugh.wav","./sounds/siegmeyermmm.wav","./sounds/spongebobhmm.wav","./sounds/Yodahmm.wav"]
-
 pygame.mixer.music.load(random.choice(hmm))
 print("pygame has loaded all the hmm")
 
 while True:
-    READ=os.read(0, 100)
+    READ=raw_input("which input\n")
     print(READ)
     choose_action(READ)
 
